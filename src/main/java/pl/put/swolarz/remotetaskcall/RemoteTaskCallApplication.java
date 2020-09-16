595b75802289a01cbc83afacc6cd7dafe8f11b72
swolarz <szymon.wol@vp.pl>
2020-09-17 00:58:51 +0200

    Initial commit Base spring gradle project

---
+++ src/main/java/pl/put/swolarz/remotetaskcall/RemoteTaskCallApplication.java
@@ -0,0 +1,13 @@
+package pl.put.swolarz.remotetaskcall;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RemoteTaskCallApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RemoteTaskCallApplication.class, args);
+	}
+
+}
