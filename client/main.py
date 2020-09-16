7b5eb4d2c6a4f52e9adaf145762d38ebbcfe46ef
swolarz <szymon.wol@vp.pl>
2020-09-17 01:05:31 +0200

    Initial service and client Move service to separate directory Create base client project

---
+++ client/main.py
@@ -0,0 +1,13 @@
+import argparse
+
+
+def main(args):
+    pass
+
+if __name__ == '__main__':
+    parser = argparse.ArgumentParser(description='Client for Remote Task Service')
+    # Todo add arguments
+
+    args = parser.parse_args()
+
+    main(args)
